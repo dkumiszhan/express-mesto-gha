f7c55c6163243e45e788a54bc05532822e6e2781
Kumiszhan Dybyspayeva <d.kumiszhan@gmail.com>
2022-09-05 00:46:51 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/mestodb");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    // у пользователя есть имя — опишем требования к имени в схеме:
+    type: String, // имя — это строка
+    required: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
+    minlength: 2, // минимальная длина имени — 2 символа
+    maxlength: 30, // а максимальная — 30 символов
+  },
+  about: {
+    // у пользователя есть имя — опишем требования к имени в схеме:
+    type: String, // имя — это строка
+    required: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
+    minlength: 2, // минимальная длина имени — 2 символа
+    maxlength: 30, // а максимальная — 30 символов
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    // у пользователя есть имя — опишем требования к имени в схеме:
+    type: String, // имя — это строка
+    required: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
+    minlength: 2, // минимальная длина имени — 2 символа
+    maxlength: 30, // а максимальная — 30 символов
+  },
+  link: {
+    // у пользователя есть имя — опишем требования к имени в схеме:
+    type: String, // имя — это строка
+    required: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
+  },
+  owner: {
+    type: mongoose.ObjectId,
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.ObjectId,
+      default: [],
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model("card", cardSchema);
+
+app.get("/users", (req, res) => {
+  res.send(req.params);
+  console.log("get users request");
+});
+
+app.get("/users/:userId", (req, res) => {
+  const { id } = req.params;
+  res.send(id);
+  console.log("get user by id request");
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
